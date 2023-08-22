1d22eb2ccffef1f58703b2902a923d1ebf2ba842
uxdiva <uxdiva@darasplace.com>
2023-08-21 21:27:14 -0700

    updates to config

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
+const markdownItEmoji = require("markdown-it-emoji");
+
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/imgs");
 
@@ -5,10 +8,43 @@ module.exports = function (eleventyConfig) {
     documentToHtmlString(value)
   );
 
-  module.exports = function(eleventyConfig) {
-    // Watch CSS files for changes
-    eleventyConfig.setBrowserSyncConfig({
-      files: './_site/css/**/*.css'
-    });
+  // Watch CSS files for changes
+  eleventyConfig.setBrowserSyncConfig({
+    files: './_site/css/**/*.css'
+  });
+  
+  eleventyConfig.addPassthroughCopy("css");
+
+  /**************** Markdown Plugins********************/
+  let markdownIt = require("markdown-it");
+  var markdownItAttrs = require('markdown-it-attrs');
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true
+  };
+  let markdownLib = markdownIt(options).use(markdownItAttrs);
+  eleventyConfig.setLibrary("md", markdownLib);
+  eleventyConfig.amendLibrary("md", mdLib => mdLib.use(markdownItEmoji));
+  return {
+    passthroughFileCopy: true,
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+
+  };
+  /**************** END Markdown Plugins********************/
+  return {
+    passthroughFileCopy: true,
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+
   };
 };
--- home.md
+++ home.md
@@ -11,15 +11,24 @@ title: Home
 ## About Us
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel libero euismod, consectetur libero nec, lacinia odio.
 
+
 ### Our Services
 - Service 1
 - Service 2
 - Service 3
 
+
+<div>
+
 ## Contact Us
 If you have any questions, please [email us](mailto:contact@example.com).
+Hello from mars :satellite:
+</div>
 
-[more](/){.btn}
-<a href="/" class="btn btn-outline">more</a>
+[more](/){.btn .btn-outline}
+
+
+
+
 
 &copy; 2023 Lorem Ipsum Page. All rights reserved.
